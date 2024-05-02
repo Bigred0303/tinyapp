4ff2fc7b6075c921704b7046c11a603d032dfb85
Grant <grantimaxi03@gmail.com>
2024-05-02 00:55:40 -0400

    Create and Display username

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 // MIDDLEWARE
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //
 
@@ -21,22 +23,24 @@ const generateRandomString = function() {
 };
 //
 
+let username = '';
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
