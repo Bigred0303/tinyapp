4e2935343267a2bb3a44793b365122ccd5cf21db
Grant <grantimaxi03@gmail.com>
2024-05-02 00:14:17 -0400

    Lint

--- express_server.js
+++ express_server.js
@@ -17,67 +17,73 @@ app.use(express.urlencoded({ extended: true }));
 // HELPER FUNCTIONS
 
 function generateRandomString() {
-    return randStr = Math.random().toString(36).slice(2, 8);
+  const randStr = null;
+  return randStr = Math.random().toString(36).slice(2, 8);
 }
 //
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls/:id", (req, res) => {
-const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-res.render("urls_show", templateVars);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
-    const longURL = urlDatabase[req.params.id]
-    res.status(200).redirect(longURL);
-  });
+  const longURL = urlDatabase[req.params.id];
+  res.status(200).redirect(longURL);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
+  res.json(urlDatabase);
+});
 
 app.get("/hello", (req, res) => {
-res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.post("/urls", (req, res) => {
 
-    const id = generateRandomString();
-    urlDatabase[id] = req.body.longURL;
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
 
-    res.status(200).redirect(`/urls/${id}`);
-  });
+  res.status(200).redirect(`/urls/${id}`);
+});
+
+// DELETE a URL
 
 app.post('/urls/:id/delete', (req, res) => {
   const id = req.params.id;
 
   delete urlDatabase[id]; // delete urlDatabase.id
 
-  res.redirect('/urls');  
+  res.redirect('/urls');
 });
+
+// EDIT a URL
 
 app.post('/urls/:id/edit', (req, res) => {
   const id = req.params.id;
 
   urlDatabase[id] = req.body.longURL; // edit urlDatabase.id
 
-  res.redirect('/urls');  
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
