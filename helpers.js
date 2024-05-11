c4a1d6a8b455b7f3ff26658e4b39b13ac94a8463
Grant <grantimaxi03@gmail.com>
2024-05-11 08:05:28 -0400

    Create tests with CHATGPT for AI Literacy

--- helpers.js
+++ helpers.js
@@ -1,18 +1,18 @@
-const getUserByEmail = function (email, users) {
-    let foundUser = undefined;
-    for (const userId in users) {
-      const user = users[userId];
-      if (user.email === email) {
-        foundUser = user;
-      }
+const getUserByEmail = function(email, users) {
+  let foundUser = undefined;
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      foundUser = user;
     }
-  
-    return foundUser
   }
+  
+  return foundUser;
+};
 
 const generateRandomString = function() {
-    return Math.random().toString(36).slice(2, 8);
+  return Math.random().toString(36).slice(2, 8);
 };
 
 
-  module.exports = { getUserByEmail, generateRandomString};
+module.exports = { getUserByEmail, generateRandomString};
