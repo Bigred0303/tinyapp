0dc24b94b471a63b45acb45dbd8c163423209970
Grant <grantimaxi03@gmail.com>
2024-05-11 06:23:13 -0400

    Switch to cookie sessions

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -13,7 +13,10 @@ app.set("view engine", "ejs");
 // MIDDLEWARE
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'MMM Session Cookies',
+  keys: ['This is a string'],
+}));
 
 //
 
@@ -54,49 +57,57 @@ const users = {
 
 };
 
+// Brings you to new URL page, must be logged in
+
 app.get("/urls/new", (req, res) => {
-  if (!req.cookies["userId"]) {
+  if (!req.session["userId"]) {
     res.redirect("/login");
   }
-  const userId = req.cookies["userId"];
+  const userId = req.session["userId"];
   const user = users[userId] ? users[userId] : null;
   const templateVars = { urls: urlDatabase, user: user };
   res.render("urls_new", templateVars);
 });
 
+// Shows you the URLS Index page, must be logged in
+
 app.get("/urls", (req, res) => {
-  if (req.cookies.userId === undefined || req.cookies.userId === null) {
-    return res.send("You cannot shorten URLS, please Login first!");
+  if (req.session.userId === undefined || req.session.userId === null) {
+    return res.status(403).send("You cannot shorten URLS, please Login first!");
   }
-  const userId = req.cookies["userId"];
+  const userId = req.session["userId"];
   const user = users[userId] ? users[userId] : null;
   const templateVars = { urls: urlsForUser(userId), user: user };
   res.render("urls_index", templateVars);
 });
 
+// Shows you the page for a specific URL, must be logged in and have the correct userID to access
+
 app.get("/urls/:id", (req, res) => {
-  if (req.cookies.userId === undefined || req.cookies.userId === null) {
-    res.send("You cannot shorten URLS, please Login first!");
+  if (req.session.userId === undefined || req.session.userId === null) {
+    res.status(403).send("You cannot shorten URLS, please Login first!");
   }
-  const userId = req.cookies["userId"];
+  const userId = req.session["userId"];
   if (userId !== urlDatabase[req.params.id].userID) {
-    res.send("This URL doesn't belong to you!");
+    res.status(403).send("This URL doesn't belong to you!");
   }
   const user = users[userId] ? users[userId] : null;
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: user };
   res.render("urls_show", templateVars);
 });
 
+// Sends you to the specified URL, must be an existing URL
+
 app.get("/u/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
-    res.send("ID does not exist silly!");
+    res.status(400).send("ID does not exist silly!");
   }
   const longURL = urlDatabase[req.params.id].longURL;
   res.status(200).redirect(longURL);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello! Please go to /urls to see TinyApp!");
+  res.send("Hello! Please go to /urls to see TinyApp! (Go to /login to skip being told to login to do anything :) )");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -107,29 +118,35 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Brings you to register page, if you already have a cookie it just sends you to /urls
+
 app.get('/register', (req, res) => {
-  if (req.cookies["userId"]) {
+  if (req.session["userId"]) {
     res.redirect("/urls");
   }
   res.render("register");
 });
 
+// Brings you to the login page, if you already have a cookie somehow it brings you to the urls page
+
 app.get('/login', (req, res) => {
-  if (req.cookies["userId"]) {
+  if (req.session["userId"]) {
     res.redirect("/urls");
   }
-  // const userId = req.cookies["userId"];
   const user = users[userId] ? users[userId] : null;
   const templateVars = { urls: urlDatabase, user: user };
   res.render("login", templateVars);
 });
 
+// Lets you create a new URL, must be logged in first
+
 app.post("/urls", (req, res) => {
-  if (req.cookies.userId === undefined || req.cookies.userId === null) {
-    res.send("You cannot shorten URLS, please Login first!");
+  if (req.session.userId === undefined || req.session.userId === null) {
+    res.status(403).send("You cannot shorten URLS, please Login first!");
   }
   const id = generateRandomString();
-  const userId = req.cookies["userId"];
+  const userId = req.session["userId"];
+  // Template object for new URLS in the database
   const newURL = {
     id: id,
     longURL: req.body.longURL,
@@ -142,8 +159,8 @@ app.post("/urls", (req, res) => {
 // DELETE a URL
 
 app.post('/urls/:id/delete', (req, res) => {
-  if (req.cookies.userId !== urlDatabase[req.params.id].userID) {
-    res.send("This URL doesn't belong to you, you can't delete it!");
+  if (req.session.userId !== urlDatabase[req.params.id].userID) {
+    res.status(403).send("This URL doesn't belong to you, you can't delete it!");
   }
   const id = req.params.id;
 
@@ -155,8 +172,8 @@ app.post('/urls/:id/delete', (req, res) => {
 // EDIT a URL
 
 app.post('/urls/:id/edit', (req, res) => {
-  if (req.cookies.userId !== urlDatabase[req.params.id].userID) {
-    res.send("This URL doesn't belong to you, you can't edit it!");
+  if (req.session.userId !== urlDatabase[req.params.id].userID) {
+    res.status(403).send("This URL doesn't belong to you, you can't edit it!");
   }
   const id = req.params.id;
 
@@ -165,8 +182,11 @@ app.post('/urls/:id/edit', (req, res) => {
   res.redirect('/urls');
 });
 
+// Login functionality
+
 app.post('/login', (req, res) => {
   let foundUser = null;
+  // Searches for your user(email) in the users object
   for (const userId in users) {
     const user = users[userId];
     if (user.email === req.body.email) {
@@ -176,30 +196,39 @@ app.post('/login', (req, res) => {
   if (!foundUser) {
     return res.status(403).send('No user with that email found');
   }
+  // Checks your plaintext password against the encrypted stored password
   if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
     return res.status(403).send('Wrong Password');
   }
 
-  res.cookie('userId', foundUser.id);
+  req.session.userId = foundUser.id;
   res.status(200).redirect('urls');
 });
 
+// Removes all cookies when you logout and sends you to login page
+
 app.post('/logout', (req, res) => {
-  res.clearCookie('userId');
+  req.session = null;
 
   res.status(200).redirect('/login');
 });
 
+// Register functionality
+
 app.post("/register" , (req, res) => {
+  // Create unique user ID
   const id = generateRandomString();
+  // Makes sure all fields are valid
   if (req.body.email === "" || req.body.password === "") {
     res.status(400).send("You left one of email or password blank, Silly!");
   }
+  // Makes sure users email doesn't already exist
   for (const user in users) {
     if (users[user].email === req.body.email) {
       res.status(400).send("A user with that email already exists, try to login instead");
     }
   }
+  // Creates new user with given information, stores password as encrypted value
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password, 10);
   const newUser = {
@@ -208,7 +237,6 @@ app.post("/register" , (req, res) => {
     password: password
   };
   users[id] = newUser;
-  console.log(users);
   res.status(200).redirect("/login");
 });
 
